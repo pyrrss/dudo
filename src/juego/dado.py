1f2fbe0b7c50fea7a5f3923751c537abbc52eeff
pyrr <oliversisaias04@gmail.com>
2025-08-21 20:19:40 -0400

    GREEN: implementación para que Dado tenga las denominaciones de las pintas del dado

--- src/juego/dado.py
+++ src/juego/dado.py
@@ -1,8 +1,20 @@
 from src.servicios.generador_aleatorio import GeneradorAleatorio
 
 class Dado:
+    denominaciones = {
+            1: "As",
+            2: "Tonto",
+            3: "Tren",
+            4: "Cuadra",
+            5: "Quina",
+            6: "Sexto"
+            }
+
     def __init__(self):
         pass
 
     def generar_valor_aleatorio(self):
         return GeneradorAleatorio.generar_valor_aleatorio()
+
+    def get_nombre_pinta(self, num_pinta):
+        return self.denominaciones[num_pinta]
