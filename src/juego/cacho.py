dd2887e2bd6b262c6e4e90ecfe04bbb16fc6dd41
blokkxz_ <cristobagonzalez2023@inf.udec.cl>
2025-08-23 01:43:31 -0400

    GREEN: implementación mínima para obtener los dados del cacho

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -1,2 +1,9 @@
+from src.juego.dado import Dado
+
+
 class Cacho:
-    pass
+    def __init__(self):
+        self.lista_de_dados = [Dado() for _ in range(5)]
+
+    def obtener_dados(self):
+        return self.lista_de_dados
