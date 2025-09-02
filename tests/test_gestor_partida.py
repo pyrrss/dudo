b1d82b0e5890274f043dd18fbcf7d19819a8b543
hooooooooola <167253637+hooooooooola@users.noreply.github.com>
2025-09-02 21:22:26 +0000

    RED: test para determinar cacho inicial - falla como esperado

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -1,6 +1,7 @@
+from unittest.mock import patch
 from src.juego.gestor_partida import GestorPartida
 
-class TestInicializacionPartida:
+class TestGestorPartida:
 
     def test_crear_partida_con_cachos(self):
         """
@@ -15,3 +16,36 @@ class TestInicializacionPartida:
             assert cacho.get_cantidad_dados() == 5
         assert gestor.cacho_actual is None
         assert gestor.direccion is None
+
+    def test_determinar_cacho_inicial_sin_empate(self, mocker):
+        """
+        el cacho con el dado mas alto inicia la partida
+        """
+        gestor = GestorPartida(3)
+    
+        # mocks para no usar testear con randoms 
+        mock_dados = [3, 6, 4]  
+    
+        with patch('src.juego.gestor_partida.GeneradorAleatorio.generar_valor_aleatorio', side_effect=mock_dados):
+            # act
+            cacho_inicial = gestor.determinar_cacho_inicial()
+    
+        # assert
+        assert cacho_inicial == gestor.cacho_actual
+
+    def test_determinar_cacho_inicial_con_empate(self, mocker):
+        """
+        en caso de empate se vuelve a tirar dados solo para los empatados
+        """
+        # Arrange
+        gestor = GestorPartida(2)
+    
+        # mock para randoms
+        mock_dados = [5, 5, 2, 6] 
+    
+        with patch('src.juego.gestor_partida.GeneradorAleatorio.generar_valor_aleatorio', side_effect=mock_dados):
+            # act
+            cacho_inicial = gestor.determinar_cacho_inicial()
+    
+        # assert
+        assert cacho_inicial == gestor.cacho_actual
